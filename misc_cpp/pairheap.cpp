382d46741735172ca8ea961c9bddd7924cf41b10
camdenSikes <gcdashsj@gmail.com>
2017-07-24 16:58:59 -0500

    Created c++ playground, started pair heap implementation

--- misc_cpp/pairheap.cpp
+++ misc_cpp/pairheap.cpp
@@ -6,22 +6,76 @@ class PairHeap{
     //To find all children, go to first child and iterate through siblings until
     //none are left
     PairHeap * firstChild;
+public:
+    void setVal(int val);
+
+    PairHeap *getFirstChild() const;
+
+    void setFirstChild(PairHeap *firstChild);
+
+    PairHeap *getSibling() const;
+
+    void setSibling(PairHeap *sibling);
+
+    PairHeap *getParent() const;
+
+    void setParent(PairHeap *parent);
+
+private:
     PairHeap * sibling;
     //not sure if I'll use this
     PairHeap * parent;
 public:
+    explicit PairHeap (int val);
     int getVal();
+
     void addChild(PairHeap heap);
     void addSibling(PairHeap heap);
 };
+
+PairHeap::PairHeap(int val) {
+    this->val = val;
+    parent = nullptr;
+    sibling = nullptr;
+    firstChild = nullptr;
+}
 
 int PairHeap::getVal(){
     return val;
 }
 
+void PairHeap::setVal(int val) {
+    PairHeap::val = val;
+}
+
+PairHeap *PairHeap::getFirstChild() const {
+    return firstChild;
+}
+
+void PairHeap::setFirstChild(PairHeap *firstChild) {
+    PairHeap::firstChild = firstChild;
+}
+
+PairHeap *PairHeap::getSibling() const {
+    return sibling;
+}
+
+void PairHeap::setSibling(PairHeap *sibling) {
+    PairHeap::sibling = sibling;
+}
+
+PairHeap *PairHeap::getParent() const {
+    return parent;
+}
+
+void PairHeap::setParent(PairHeap *parent) {
+    PairHeap::parent = parent;
+}
+
 //Make your child add a sibling
 void PairHeap::addChild(PairHeap heap) {
-    firstChild->addSibling(heap);
+    heap.setParent(this);
+    firstChild == nullptr ? firstChild = &heap : firstChild->addSibling(heap);
 }
 
 //If you don't point to a sibling, add this
@@ -37,6 +91,11 @@ int findMin(PairHeap heap){
 PairHeap merge(PairHeap h1, PairHeap h2){
     //TODO: deal with one being empty
     (h1.getVal() > h2.getVal()) ? h1.addChild(h2) : h2.addChild(h1);
+}
+
+PairHeap insert(PairHeap heap, int val){
+    PairHeap newHeap {val};
+    return merge(heap, newHeap);
 }
 
 int main() {
